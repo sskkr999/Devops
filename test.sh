f1e1f27b3f60b8bc7f33c1085d45a0d08ac3959f
saikirankumar <saikirankumar@gmail.com>
2025-01-31 11:15:37 +0530

    added finally

---
+++ test.sh
@@ -0,0 +1,24 @@
+mkdir naresh
+cd naresh
+echo hi file1>> f1
+mkdir sunny
+cd sunny
+echo hello sunny >> f2
+yum install java -y
+echo successfully completed
+yum install wget -y
+yum install unzip -y
+yum install vim -y
+yum install epel-release -y
+yum install ansible -y
+yum install git -y
+yum install maven -y
+yum install python -y
+yum install python-pip -y
+yum install tree -y
+yum install java -y
+yum install nginx -y
+ansible --version
+java -version
+echo successfully completed
+
